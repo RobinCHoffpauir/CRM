93c64e61bc8b58a64d50313d597c9bdceabf4236
Robin C. Hoffpauir <91177807+RobinCHoffpauir@users.noreply.github.com>
2022-05-01 06:44:48 -0500

    setup.py upload

---
+++ setup.py
@@ -0,0 +1,313 @@
+from tkinter import *
+import sqlite3
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkmessagebox
+
+root = Tk()
+root.title("Customer Management System")
+width = 700
+height = 400
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+root.config(bg="#F62A00")
+
+#============================VARIABLES===================================
+FIRSTNAME = StringVar()
+LASTNAME = StringVar()
+GENDER = StringVar()
+COMPANY = StringVar()
+PRIORITY = StringVar()
+CONTACT = StringVar()
+HOT_COLD = StringVar()
+
+
+
+
+#============================METHODS=====================================
+
+def Database():
+    conn = sqlite3.connect("CRMdatabase.db")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS `member` (mem_id INTEGER NOT NULL  PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, gender TEXT, company TEXT, priority TEXT, contact TEXT, hot_cold TEXT)")
+    cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
+    fetch = cursor.fetchall()
+    for data in fetch:
+        tree.insert('', 'end', values=(data))
+    cursor.close()
+    conn.close()
+
+def SubmitData():
+    if  FIRSTNAME.get() == "" or LASTNAME.get() == "" or GENDER.get() == "" or COMPANY.get() == "" or PRIORITY.get() == "" or CONTACT.get() == "" or HOT_COLD.get() == "":
+        result = tkmessagebox.showwarning('', 'Please Complete The Required Field', icon="warning")
+    else:
+        tree.delete(*tree.get_children())
+        conn = sqlite3.connect("CRMdatabase.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO `member` (firstname, lastname, gender, company, priority, contact) VALUES(?, ?, ?, ?, ?, ?)", (str(FIRSTNAME.get()), str(LASTNAME.get()), str(GENDER.get()), str(COMPANY.get()), str(PRIORITY.get()), str(CONTACT.get())))
+        conn.commit()
+        cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
+        fetch = cursor.fetchall()
+        for data in fetch:
+            tree.insert('', 'end', values=(data))
+        cursor.close()
+        conn.close()
+        FIRSTNAME.set("")
+        LASTNAME.set("")
+        GENDER.set("")
+        COMPANY.set("")
+        PRIORITY.set("")
+        CONTACT.set("")
+        HOT_COLD.set("")
+def UpdateData():
+    if GENDER.get() == "":
+       result = tkmessagebox.showwarning('', 'Please Complete The Required Field', icon="warning")
+    else:
+        tree.delete(*tree.get_children())
+        conn = sqlite3.connect("CRMdatabase.db")
+        cursor = conn.cursor()
+        cursor.execute("UPDATE `member` SET `firstname` = ?, `lastname` = ?, 'gender' = ?, `company` = ?,  `priority` = ?, `contact` = ? WHERE `mem_id` = ?", (str(FIRSTNAME.get()), str(LASTNAME.get()), str(GENDER.get()), str(COMPANY.get()), str(PRIORITY.get()), str(CONTACT.get()), str(HOT_COLD.get()), int(mem_id)))
+        conn.commit()
+        cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
+        fetch = cursor.fetchall()
+        for data in fetch:
+            tree.insert('', 'end', values=(data))
+        cursor.close()
+        conn.close()
+        FIRSTNAME.set("")
+        LASTNAME.set("")
+        GENDER.set("")
+        COMPANY.set("")
+        PRIORITY.set("")
+        CONTACT.set("")
+        HOT_COLD.set("")
+
+def OnSelected(event):
+    global mem_id, UpdateWindow
+    curItem = tree.focus()
+    contents =(tree.item(curItem))
+    selecteditem = contents['values']
+    mem_id = selecteditem[0]
+    FIRSTNAME.set("")
+    LASTNAME.set("")
+    GENDER.set("")
+    COMPANY.set("")
+    PRIORITY.set("")
+    CONTACT.set("")
+    HOT_COLD.set("")
+    FIRSTNAME.set(selecteditem[1])
+    LASTNAME.set(selecteditem[2])
+    GENDER.set(selecteditem[3])
+    COMPANY.set(selecteditem[4])
+    PRIORITY.set(selecteditem[5])
+    CONTACT.set(selecteditem[6])
+    HOT_COLD.set(selecteditem[7])
+    UpdateWindow = Toplevel()
+    UpdateWindow.title("Customer Management System")
+    width = 400
+    height = 300
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = ((screen_width/2) + 450) - (width/2)
+    y = ((screen_height/2) + 20) - (height/2)
+    UpdateWindow.resizable(0, 0)
+    UpdateWindow.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    if 'NewWindow' in globals():
+        NewWindow.destroy()
+
+    #===================FRAMES==============================
+    FormTitle = Frame(UpdateWindow)
+    FormTitle.pack(side=TOP)
+    ContactForm = Frame(UpdateWindow)
+    ContactForm.pack(side=TOP, pady=10)
+
+    #===================LABELS==============================
+    lbl_title = Label(FormTitle, text="Updating Contacts", font=('arial', 16), bg="#F1F3CE",  width = 300)
+    lbl_title.pack(fill=X)
+    lbl_firstname = Label(ContactForm, text="Firstname", font=('arial', 14), bd=5)
+    lbl_firstname.grid(row=0, sticky=W)
+    lbl_lastname = Label(ContactForm, text="Lastname", font=('arial', 14), bd=5)
+    lbl_lastname.grid(row=1, sticky=W)
+    lbl_gender = Label(ContactForm, text='Gender', font=('arial', 14), bd=5)
+    lbl_gender.grid(row=2, sticky=W)
+    lbl_company = Label(ContactForm, text="Company", font=('arial', 14), bd=5)
+    lbl_company.grid(row=3, sticky=W)
+    lbl_priority = Label(ContactForm, text="Priority", font=('arial', 14), bd=5)
+    lbl_priority.grid(row=4, sticky=W)
+    lbl_contact = Label(ContactForm, text="Contact", font=('arial', 14), bd=5)
+    lbl_contact.grid(row=5, sticky=W)
+    lbl_hot_cold = Label(ContactForm, text="Hot_cold", font=('arial', 14), bd=5)
+    lbl_hot_cold.grid(row=6, sticky=W)
+
+
+    #===================ENTRY===============================
+    firstname = Entry(ContactForm, textvariable=FIRSTNAME, font=('arial', 14))
+    firstname.grid(row=0, column=1)
+    lastname = Entry(ContactForm, textvariable=LASTNAME, font=('arial', 14))
+    lastname.grid(row=1, column=1)
+    gender = Entry(ContactForm, textvariable=GENDER, font = ('arial', 14))
+    gender.grid(row=2, column=1)
+    company = Entry(ContactForm, textvariable=COMPANY,  font=('arial', 14))
+    company.grid(row=3, column=1)
+    priority = Entry(ContactForm, textvariable=PRIORITY,  font=('arial', 14))
+    priority.grid(row=4, column=1)
+    contact = Entry(ContactForm, textvariable=CONTACT,  font=('arial', 14))
+    contact.grid(row=5, column=1)
+    hot_cold = Entry(ContactForm, textvariable=HOT_COLD,  font=('arial', 14))
+    hot_cold.grid(row=6, column=1)
+
+
+    #==================BUTTONS==============================
+    btn_updatecon = Button(ContactForm, text="Update", width=50, command=UpdateData)
+    btn_updatecon.grid(row=7, columnspan=2, pady=10)
+
+
+#fn1353p
+def DeleteData():
+    if not tree.selection():
+       result = tkmessagebox.showwarning('', 'Please Select Something First!', icon="warning")
+    else:
+        result = tkmessagebox.askquestion('', 'Are you sure you want to delete this record?', icon="warning")
+        if result == 'yes':
+            curItem = tree.focus()
+            contents =(tree.item(curItem))
+            selecteditem = contents['values']
+            tree.delete(curItem)
+            conn = sqlite3.connect("CRMdatabase.db")
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM `member` WHERE `mem_id` = %d" % selecteditem[0])
+            conn.commit()
+            cursor.close()
+            conn.close()
+
+def AddNewWindow():
+    global NewWindow
+    FIRSTNAME.set("")
+    LASTNAME.set("")
+    GENDER.set("")
+    COMPANY.set("")
+    PRIORITY.set("")
+    CONTACT.set("")
+    HOT_COLD.set("")
+    NewWindow = Toplevel()
+    NewWindow.title("Customer Management System")
+    width = 400
+    height = 300
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = ((screen_width/2) - 455) - (width/2)
+    y = ((screen_height/2) + 20) - (height/2)
+    NewWindow.resizable(0, 0)
+    NewWindow.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    if 'UpdateWindow' in globals():
+        UpdateWindow.destroy()
+
+    #===================FRAMES==============================
+    FormTitle = Frame(NewWindow)
+    FormTitle.pack(side=TOP)
+    ContactForm = Frame(NewWindow)
+    ContactForm.pack(side=TOP, pady=10)
+    #===================LABELS==============================
+    lbl_title = Label(FormTitle, text="Adding New Customer", font=('arial', 16), bg="#F1F3CE",  width = 300)
+    lbl_title.pack(fill=X)
+    lbl_firstname = Label(ContactForm, text="Firstname", font=('arial', 14), bd=5)
+    lbl_firstname.grid(row=0, sticky=W)
+    lbl_lastname = Label(ContactForm, text="Lastname", font=('arial', 14), bd=5)
+    lbl_lastname.grid(row=1, sticky=W)
+    lbl_gender = Label(ContactForm, text="Gender", font=('arial', 14), bd=5)
+    lbl_gender.grid(row=2, sticky=W)
+    lbl_COMPANY = Label(ContactForm, text="Company", font=('arial', 14), bd=5)
+    lbl_COMPANY.grid(row=3, sticky=W)
+    lbl_PRIORITY = Label(ContactForm, text="Priority", font=('arial', 14), bd=5)
+    lbl_PRIORITY.grid(row=4, sticky=W)
+    lbl_contact = Label(ContactForm, text="Contact", font=('arial', 14), bd=5)
+    lbl_contact.grid(row=5, sticky=W)
+    lbl_hot_cold = Label(ContactForm, text="Hot_cold", font=('arial', 14), bd=5)
+    lbl_hot_cold.grid(row=6, sticky=W)
+
+    #===================ENTRY===============================
+    firstname = Entry(ContactForm, textvariable=FIRSTNAME, font=('arial', 14))
+    firstname.grid(row=0, column=1)
+    lastname = Entry(ContactForm, textvariable=LASTNAME, font=('arial', 14))
+    lastname.grid(row=1, column=1)
+    gender = Entry(ContactForm, textvariable=GENDER, font = ('arial', 14))
+    gender.grid(row=2, column=1)
+
+
+    company = Entry(ContactForm, textvariable=COMPANY,  font=('arial', 14))
+    company.grid(row=3, column=1)
+    priority = Entry(ContactForm, textvariable=PRIORITY,  font=('arial', 14))
+    priority.grid(row=4, column=1)
+    contact = Entry(ContactForm, textvariable=CONTACT,  font=('arial', 14))
+    contact.grid(row=5, column=1)
+    hot_cold = Entry(ContactForm, textvariable=HOT_COLD,  font=('arial', 14))
+    hot_cold.grid(row=6, column=1)
+
+
+    #==================BUTTONS==============================
+    btn_addcon = Button(ContactForm, text="Save", width=50, command=SubmitData)
+    btn_addcon.grid(row=7, columnspan=2, pady=10)
+
+
+
+
+
+#============================FRAMES======================================
+Top = Frame(root, width=500, bd=1, relief=SOLID)
+Top.pack(side=TOP)
+Mid = Frame(root, width=500,  bg="#1E656D")
+Mid.pack(side=TOP)
+MidLeft = Frame(Mid, width=100)
+MidLeft.pack(side=LEFT, pady=10)
+MidLeftPadding = Frame(Mid, width=370, bg="#1E656D")
+MidLeftPadding.pack(side=LEFT)
+MidRight = Frame(Mid, width=100)
+MidRight.pack(side=RIGHT, pady=10)
+TableMargin = Frame(root, width=500)
+TableMargin.pack(side=TOP)
+#============================LABELS======================================
+lbl_title = Label(Top, text="Robin C. Hoffpauir's Sales Funnel", font=('arial', 16), width=500,bg="black")
+lbl_title.pack(fill=X)
+
+#============================ENTRY=======================================
+
+#============================BUTTONS=====================================
+btn_add = Button(MidLeft, text="+ ADD NEW", bg="#F62A00", command=AddNewWindow)
+btn_add.pack()
+btn_delete = Button(MidRight, text="DELETE", bg="#F62A00", command=DeleteData)
+btn_delete.pack(side=RIGHT)
+
+#============================TABLES======================================
+scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
+scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
+tree = ttk.Treeview(TableMargin, columns=("MemberID", "Firstname", "Lastname", "Gender", "Company", "Priority", "Contact"), height=400, selectmode="extended", yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+scrollbary.config(command=tree.yview)
+scrollbary.pack(side=RIGHT, fill=Y)
+scrollbarx.config(command=tree.xview)
+scrollbarx.pack(side=BOTTOM, fill=X)
+tree.heading('MemberID', text="MemberID", anchor=W)
+tree.heading('Firstname', text="Firstname", anchor=W)
+tree.heading('Lastname', text="Lastname", anchor=W)
+tree.heading('Gender', text="Gender", anchor=W)
+tree.heading('Company', text="Company", anchor=W)
+tree.heading('Priority', text="Priority", anchor=W)
+tree.heading('Contact', text="Contact", anchor=W)
+tree.column('#0', stretch=NO, minwidth=0, width=0)
+tree.column('#1', stretch=NO, minwidth=0, width=0)
+tree.column('#2', stretch=NO, minwidth=0, width=80)
+tree.column('#3', stretch=NO, minwidth=0, width=120)
+tree.column('#4', stretch=NO, minwidth=0, width=90)
+tree.column('#5', stretch=NO, minwidth=0, width=80)
+tree.column('#6', stretch=NO, minwidth=0, width=120)
+tree.column('#7', stretch=NO, minwidth=0, width=120)
+
+tree.pack()
+tree.bind('<Double-Button-1>', OnSelected)
+
+#============================INITIALIZATION==============================
+if __name__ == '__main__':
+    Database()
+    root.mainloop()
